b8bf14df2b2d6d2ef9dd0e1f54cd047fc105a9bd
twined quartz <fmorisato@gmail.com>
2018-05-17 16:12:21 +0900

    move partitioning coefficient

---
+++ js/partitioning.js
@@ -0,0 +1,64 @@
+(function (root, factory) {
+  if (typeof define === 'function' && define.amd) {
+    define([], factory);
+  } else if (typeof exports === 'object') {
+    module.exports = factory();
+  } else {
+    root.partitioning = factory();
+  }
+}(this, function () {
+  const olivine = {
+    Al2O3: {
+      myCompile: liquid => (T, P = 0) => 0,
+      dummy: liquid => (T, P) => 0
+    },
+    CaO: {
+      myCompile: liquid => (T, P = 0) => 0,
+      dummy: liquid => (T, P) => 0
+    },
+    TiO2: {
+      dummy: liquid => (T, P) => 0
+    },
+    Cr2O3: {
+      FreiS2009: liquid => (T, P) => 1.2
+    },
+    NiO: {
+      NormanS2005: liquid => (T, P) => 10
+    }
+  };
+
+  const orthopyroxene = {
+    Al2O3: {
+      myCompile: liquid => (T, P = 0) => (-0.0043 * P - 0.0052) * liquid.major.MgO + (0.1669 * P + 0.1407),
+      dummy: liquid => (T, P) => 0
+    },
+    CaO: {
+      myCompile: liquid => (T, P = 0) => (0.0083 * P - 0.0189) * liquid.major.MgO + (-0.0451 * P + 0.3185),
+      dummy: liquid => (T, P) => 0
+    },
+    TiO2: {
+      dummy: liquid => (T, P) => 0
+    },
+    Cr2O3: {
+      FreiS2009: liquid => (T, P) => 4
+    },
+    NiO: {
+      NormanS2005: liquid => (T, P) => 5
+    }
+  };
+
+  const spinel = {
+    Cr2O3: {
+      LieS2008: liquid => (T, P) => 130
+    },
+    NiO: {
+      LieS2008: liquid => (T, P) => 2
+    }
+  };
+
+  return {
+    olivine,
+    orthopyroxene,
+    spinel
+  };
+}))
